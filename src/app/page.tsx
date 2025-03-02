22c9a861053aa7f46b596b4e879310056628cf1f
Nicolas Badin <50690360+N1coc4colA@users.noreply.github.com>
2025-03-02 23:14:51 +0100

    Finish tuto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
-import { Button } from "@/components/ui/button";
-import { ModeToggle } from "@/components/ModeToggle";
+import { CreatePost } from "@/components/CreatePost";
+import { PostCard } from "@/components/PostCard";
+import { WhoToFollow } from "@/components/WhoToFollow";
+import { currentUser } from "@clerk/nextjs/server";
+import { getPosts } from "@/actions/post.action";
+import { getDbUserId } from "@/actions/user.action";
 
-export default function HomePage() {
-  return (
-    <h1> Welcome !</h1>
-  );
+export default async function HomePage() {
+    const user = await currentUser();
+    const posts = await getPosts();
+    const dbUserId = await getDbUserId();
+
+    return (
+        <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+            <div className="lg:col-span-6">
+                {user ? <CreatePost /> : null}
+
+                <div className="space-y-6">
+                    {posts.map((post) => (
+                      <PostCard key={post.id} post={post} dbUserId={dbUserId} />
+                    ))}
+                </div>
+            </div>
+
+            <div className="hidden lg:block lg:col-span-4 sticky top-20">
+                <WhoToFollow />
+            </div>
+        </div>
+    );
 }
